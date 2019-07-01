50c61a42746d96368ba087ec0618f7f70c33015c
unknown <Alao-Abiodun>
2019-07-01 04:52:08 +0100

    Add database for userAPI

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a database.
+  res.json([
+    {"id":1,"firstName":"Alao","lastName":"Abiodun","email":"alao@gmail.com"},
+    {"id":2,"firstName":"Ajibola","lastName":"Habeeb","email":"hab@gmail.com"},
+    {"id":3,"firstName":"Jamiu","lastName":"Nofiu","email":"Nofiu@gmail.com"}
+  ]);
+});
+
 
 app.listen(port, (err) => {
   if(err){
